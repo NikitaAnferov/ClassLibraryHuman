97e6e088cd12f75356e5305e0dd494ae58a6d848
nikitaanferov <nikitaanferov389@gmail.com>
2021-06-20 14:03:31 +0700

    Add project files.

---
+++ ClassLibraryHuman/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryHuman
+{
+    public abstract class Human
+    {
+        public string fio { get; set; }
+        Gender gen;
+        public enum Gender { мужской, женский};
+        public DateTime birthdate { get; set; }
+
+        public Human()
+        {
+            fio = "Unknown";
+            gen = Gender.мужской;
+            birthdate = new DateTime(1900, 01, 01);
+        }
+
+        public Human(string humfio, Gender g, DateTime bd)
+        {
+            fio = humfio;
+            gen = g;
+            birthdate = bd;
+        }
+
+        public abstract string GetInfo();
+    }
+}
